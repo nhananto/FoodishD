dfd2e12e50a87b3d9ba1d883ae5640599189f15d
soniaakhter <67204193+soniaakhter@users.noreply.github.com>
2020-09-27 00:08:41 +0600

    templates_index

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
 
     path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('tour', views.tour, name='tour'),
+    path('contact', views.contact, name='contact'),
     path('404/', views.not_found, name='not_found')
 
 
--- main/views.py
+++ main/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from main.models import *
 
 
 def index(request):
-    return render(request, 'main/index.html')
+
+    all_post = Post.objects.all() 
+
+    context = {
+        'posts' : all_post
+    }
+    return render(request, 'main/index.html', context)
 
 
 def not_found(request):
     return render(request, 'main/404.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def tour(request):
+    return render(request, 'main/tours.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
